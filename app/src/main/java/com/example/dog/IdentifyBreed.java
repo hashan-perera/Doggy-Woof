13727f48a8a31a1f581b603ee1c4a15bafc096ba
Supun Perera <supunperera@Supuns-MacBook-Pro.local>
2020-03-03 15:57:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dog/IdentifyBreed.java
@@ -0,0 +1,14 @@
+package com.example.dog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IdentifyBreed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_identify_breed);
+    }
+}
---
+++ app/src/main/java/com/example/dog/SearchDogBreed.java
@@ -0,0 +1,14 @@
+package com.example.dog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchDogBreed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_dog_breed);
+    }
+}
