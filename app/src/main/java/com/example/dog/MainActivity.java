8667933f00ecd92e1b992c3c6744baba6b3bd8e5
supunperera <7hashd@gmail.com>
2020-03-04 20:21:27 +0530

    Initial commit

--- app/src/main/java/com/example/dog/MainActivity.java
+++ app/src/main/java/com/example/dog/MainActivity.java
@@ -2,13 +2,57 @@ package com.example.dog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    //LogCat field.
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //LogCat statement.
+        Log.d(LOG_TAG, "On create.");
+
+        final Button identifyBreed = (Button) findViewById(R.id.btn_breed);
+        Button identifyDog = (Button) findViewById(R.id.btn_dog);
+        Button search = (Button) findViewById(R.id.btn_search);
+
+        //Button Identify The Breed.
+        identifyBreed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), IdentifyBreed.class);
+                startActivity(intent);
+                //LogCat statement.
+                Log.d(LOG_TAG, "Button clicked, Identify The Breed.");
+
+            }
+        });
+        //Button Identify The Dog.
+        identifyDog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), IdentifyDog.class);
+                startActivity(intent);
+                //LogCat statement.
+                Log.d(LOG_TAG, "Button clicked, Identify The Dog.");
+            }
+        });
+        //Button Search Dog Breed.
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SearchDogBreed.class);
+                startActivity(intent);
+                //LogCat statement.
+                Log.d(LOG_TAG, "Button clicked, Search Dog Breed.");
+            }
+        });
     }
 }
