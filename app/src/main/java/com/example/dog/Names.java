7a83ffd3517b7880048b3fbd0ec7cbe68a4ddcbc
supunperera <7hashd@gmail.com>
2020-03-06 14:12:50 +0530

    Initial commit

--- app/src/main/java/com/example/dog/Names.java
+++ app/src/main/java/com/example/dog/Names.java
@@ -1,4 +1,17 @@
 package com.example.dog;
 
 public class Names {
+    String[] nameArray = new String[]
+            {
+                    "American Staffordshire Terrier",
+                    "Basset",
+                    "Beagle",
+                    "Boxer",
+                    "Bull Mastiff",
+                    "Doberman",
+                    "German Shepherd",
+                    "Pomeranian",
+                    "Pug",
+                    "Rhodesian Ridgeback",
+            };
 }
