e6e3d7ccb8d4a3c555f724e6d4c431a98935dded
supunperera <7hashd@gmail.com>
2020-03-09 10:35:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dog/Search.java
@@ -0,0 +1,37 @@
+package com.example.dog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+public class Search extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        SearchView searchView = findViewById(R.id.search);
+        ListView listView=findViewById(R.id.list);
+        Names names=new Names();
+
+        final ArrayAdapter<String> adapter =new ArrayAdapter<String>(Search.this, android.R.layout.simple_dropdown_item_1line, names.nameArray);
+        listView.setAdapter(adapter);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                adapter.getFilter().filter(s);
+                return false;
+            }
+        });
+    }
+}
