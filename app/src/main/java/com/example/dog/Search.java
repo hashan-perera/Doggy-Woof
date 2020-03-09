2efbf4c2176db71798956787ea4f8572c564daf9
supunperera <7hashd@gmail.com>
2020-03-09 22:57:57 +0530

    Initial commit

--- app/src/main/java/com/example/dog/Search.java
+++ app/src/main/java/com/example/dog/Search.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 public class Search extends AppCompatActivity {
 
@@ -14,16 +17,25 @@ public class Search extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        SearchView searchView = findViewById(R.id.search);
-        ListView listView=findViewById(R.id.list);
-        Names names=new Names();
+        final Names names = new Names();
+        SearchView search = findViewById(R.id.search);
+        final ListView list = findViewById(R.id.list);
+        Button button_submit3=findViewById(R.id.button_submit3);
+        ImageView imageview_dog3=findViewById(R.id.imageview_dog3);
+        Button button_stop=findViewById(R.id.button_stop);
 
-        final ArrayAdapter<String> adapter =new ArrayAdapter<String>(Search.this, android.R.layout.simple_dropdown_item_1line, names.nameArray);
-        listView.setAdapter(adapter);
 
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(Search.this, android.R.layout.simple_list_item_1, names.nameArray);
+        list.setAdapter(adapter);
+
+        search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
+                if(names.nameArray.equals(s)){
+                    adapter.getFilter().filter(s);
+                }else{
+                    Toast.makeText(Search.this, "No Match found",Toast.LENGTH_LONG).show();
+                }
                 return false;
             }
 
@@ -32,6 +44,8 @@ public class Search extends AppCompatActivity {
                 adapter.getFilter().filter(s);
                 return false;
             }
+
+
         });
     }
 }
