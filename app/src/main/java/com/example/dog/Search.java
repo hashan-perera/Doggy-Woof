369b4a5aa842e714db19f9242d664ad689077e43
supunperera <7hashd@gmail.com>
2020-03-10 11:32:01 +0530

    Initial commit

--- app/src/main/java/com/example/dog/Search.java
+++ app/src/main/java/com/example/dog/Search.java
@@ -18,11 +18,12 @@ public class Search extends AppCompatActivity {
         setContentView(R.layout.activity_search);
 
         final Names names = new Names();
-        SearchView search = findViewById(R.id.search);
+        final Names2 names2 = new Names2();
+        final SearchView search = findViewById(R.id.search);
         final ListView list = findViewById(R.id.list);
-        Button button_submit3=findViewById(R.id.button_submit3);
-        ImageView imageview_dog3=findViewById(R.id.imageview_dog3);
-        Button button_stop=findViewById(R.id.button_stop);
+        Button button_submit3 = findViewById(R.id.button_submit3);
+        final ImageView imageview_dog3 = findViewById(R.id.imageview_dog3);
+        Button button_stop = findViewById(R.id.button_stop);
 
 
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>(Search.this, android.R.layout.simple_list_item_1, names.nameArray);
@@ -31,10 +32,20 @@ public class Search extends AppCompatActivity {
         search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
-                if(names.nameArray.equals(s)){
+                if (names.nameArray.equals(s)) {
                     adapter.getFilter().filter(s);
-                }else{
-                    Toast.makeText(Search.this, "No Match found",Toast.LENGTH_LONG).show();
+
+                    for (int i = 0; i >= 0 && i <= 119; i++) {
+                        String searchName = (String) search.getQuery();
+                        String arrayName = names2.nameArray2[i];
+                        if (searchName == arrayName) {
+                            int imageIndex = i;
+                        }
+                    }
+
+
+                } else {
+                    Toast.makeText(Search.this, "No Match found", Toast.LENGTH_LONG).show();
                 }
                 return false;
             }
